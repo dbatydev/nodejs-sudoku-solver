ebfbae53ab6a530d13b82b2a44a28e3da2038f68
david <dbaty1989@gmail.com>
2021-07-28 08:30:03 -0400

    simplify appjs

--- app.js
+++ app.js
@@ -1,84 +1,45 @@
-/**
- * @var {Array} board - an array of arrays representing a sudoku board
- */
-let board = [
-  [0, 9, 0, 2, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 1, 0, 9, 0, 8],
-  [6, 0, 8, 0, 0, 0, 7, 1, 0],
-  [0, 0, 0, 0, 8, 0, 0, 7, 0],
-  [3, 8, 0, 5, 6, 0, 0, 0, 0],
-  [2, 0, 9, 1, 0, 0, 0, 0, 5],
-  [0, 0, 0, 0, 2, 3, 0, 6, 0],
-  [4, 6, 2, 0, 5, 1, 8, 0, 3],
-  [8, 1, 0, 0, 4, 9, 5, 2, 7],
+let grid = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ];
-
+//think x and y axis are mixed up
 /**
- * nextEmptySpot()
- * Finds the first empty spot or 0
- * @param   {Array} board - Sudoku board
- * @returns {Array} - an array of the next empty spot, if none available returns
+ * isPossible()
+ *
+ * @param   {int}   y - The y axis or vertical column wanting to check
+ * @param   {int}   x - The x axis or horizontal row wanting to check
+ * @param   {int}   n - The number to check against
+ * @returns {Bool}  true/false if number passed in will work in the cell
  */
-const nextEmptySpot = (board) => {
-  for (const foo of board.keys()) {
-    for (const bar of board.keys()) {
-      if (board[foo][bar] === 0) {
-        return [foo, bar];
-      }
+const isPossible = (y, x, n) => {
+  //y axis
+  /*for (const i of grid.keys()) {
+    if (grid[y][i] == n) {
+      return false;
     }
-  }
-  return [-1, -1];
-};
+  }*/
+  if (grid[y].includes(n)) return false;
 
-/**
- * checkRow()
- * Function to validate row, it takes in the board, the row we want to check, and new val we want to add to this row, iterates over passed in row and returns false if contains passed in value
- * @param {Array} board - Sudoku board
- * @param {Array} row   - Row want to check
- * @param {Int}   value - Value want to add to the row
- * @returns {Boolean} - If row contains value wanted to add false else true
- */
-const checkRow = (board, row, value) => {
-  for (const i of board[row].keys()) {
-    if (board[row][i] === value) {
+  //x axis
+  for (const j of grid.keys()) {
+    if (grid[j][x] == n) {
       return false;
     }
   }
-  return true;
-};
+  gridRow = Math.floor(x / 3) * 3;
+  gridCol = Math.floor(y / 3) * 3;
 
-/**
- * checkColumn()
- * Function to validate column, it takes in the board, the column we want to check, and new val we want to add to this column, iterates over passed in column and returns false if contains passed in value
- * @param {Array} board - Sudoku board
- * @param {Array} column   - column want to check
- * @param {Int}   value - Value want to add to the column
- * @returns {Boolean} - If row contains value wanted to add false else true
- */
-const checkColumn = (board, column, value) => {
-  for (const i of board.keys()) {
-    if (board[i][column] === value) {
-      return false;
-    }
-  }
-  return true;
-};
-
-/**
- * checkSquare()
- * Creates 3x3 square and iterates checking for existing val returns false if yes else true
- * @param   {Array} board   - Sudoku board
- * @param   {Array} row     - The row to check
- * @param   {Array} column  - The col to check
- * @param   {Int}   value   - The value to check against the previous params
- * @returns {Bool}  - if existing val false else true
- */
-const checkSquare = (board, row, column, value) => {
-  boxRow = Math.floor(row / 3) * 3;
-  boxCol = Math.floor(column / 3) * 3;
+  //check the square
   for (let r = 0; r < 3; r++) {
     for (let c = 0; c < 3; c++) {
-      if (board[boxRow + r][boxCol + c] === value) {
+      if (grid[gridCol + r][gridRow + c] === n) {
         return false;
       }
     }
@@ -87,56 +48,28 @@ const checkSquare = (board, row, column, value) => {
 };
 
 /**
- * checkValues()
- * Helper function that calls calls the checkRow, checkColumn and checkSquare
- * @param   {Array} board   - Sudoku board
- * @param   {Array} row     - The row to check
- * @param   {Array} column  - The col to check
- * @param   {Int}   value   - The value to check against the previous params
- * @returns {Bool} - returns true if checkRow, checkColumn and checkSquare return true else false
- */
-const checkValues = (board, row, column, value) => {
-  if (
-    checkRow(board, row, value) &&
-    checkColumn(board, column, value) &&
-    checkSquare(board, row, column, value)
-  ) {
-    return true;
-  }
-
-  return false;
-};
-
-/**
  * solvePuzzle()
- * Function that solves the board passed in, it checks all values and makes a recursive function call to recheck, thereby using the Backtracking algorithm
- * @see https://en.wikipedia.org/wiki/Backtracking
- * @param {Array} board - Sudoku board
- * @returns {Array} - an array of the solved sudoku puzzle
+ * Iterates the y axis, then the x axis to get array position, iterates a for loop
+ * as n "1-9" to see if isPossible() and sets grid array position to n. finally it uses the
+ * backtrack algorithm by calling solvePuzzle() recursively
+ * @returns  {Array} - prints solved grid as array to the console
  */
-const solvePuzzle = (board) => {
-  let emptySpot = nextEmptySpot(board);
-  let row = emptySpot[0];
-  let col = emptySpot[1];
-
-  //no empty inputs/places exist
-  if (row === -1) {
-    return board;
-  }
-
-  for (let num = 1; num <= 9; num++) {
-    if (checkValues(board, row, col, num)) {
-      board[row][col] = num;
-      solvePuzzle(board);
+const solvePuzzle = () => {
+  for (let y of grid.keys()) {
+    for (let x of grid.keys()) {
+      if (grid[y][x] == 0) {
+        for (let n = 1; n < 10; n++) {
+          if (isPossible(y, x, n)) {
+            grid[y][x] = n; //set empty arr element or cell to n
+            solvePuzzle(); //recursive
+            grid[y][x] = 0;
+          }
+        }
+        return;
+      }
     }
   }
-
-  if (nextEmptySpot(board)[0] !== -1) {
-    board[row][col] = 0;
-  }
-
-  return board;
+  console.log(grid);
 };
 
-//answer
-console.log(solvePuzzle(board));
+solvePuzzle();
